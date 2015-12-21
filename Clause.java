1749cb6e09326facb9d2527c456159f0fa706789
Stavan <stavan.karia@gmail.com>
2015-12-21 18:53:07 -0500

    Added comments and formatting

--- Clause.java
+++ Clause.java
@@ -1,20 +1,26 @@
+// necssary imports for thee program
 import java.util.*;
+
 // Clause class
 // the objects store each clause as a hashset
 public class Clause{
 	public Set<Integer> m_literal;
+
 	// empty contructor
 	public Clause(){
 		m_literal = new HashSet<Integer>();
 	}
+
 	// check if the clause is empty 
 	public boolean clauseIsEmpty(){
 		return (m_literal.size()==0);
 	}
+
 	// check if clause is unit clause
 	public boolean clauseunity(){
 		return (m_literal.size() == 1);
 	}
+
 	// get literal from a clause
 	public int getLiteral() throws IndexOutOfBoundsException{
 		if (clauseIsEmpty())
@@ -24,14 +30,17 @@ public class Clause{
 		Iterator iterator = m_literal.iterator();	
 		return (int)iterator.next();
 	}
+
 	// add literal to a clause
 	public void addLiteral(int i){
 		m_literal.add((int)i);
 	}
+
 	// remove literal from a clause
 	public void rmvLiteral(int i) {
 		m_literal.remove((Integer)i);
 	}
+
 	// check if clause has a literal
 	public boolean hasLiteral(int i){
 		Iterator iterator = m_literal.iterator();		
@@ -42,6 +51,7 @@ public class Clause{
 		}
 		return false;
 	}	
+
 	// convert hashset to a object array
 	public Object[] clauseToString(){
 		Object[] value = m_literal.toArray();
